6c4e3d8f92a68369bcceba4a37b36b770e0b061e
yavor4o <ydobrev@abv.bg>
2024-03-14 13:14:13 +0200

    New

--- dobbox_web/authentication/forms.py
+++ dobbox_web/authentication/forms.py
@@ -1,7 +1,10 @@
+import form
 from django import forms
 from django.contrib.auth import get_user_model, password_validation
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm, PasswordChangeForm, \
     SetPasswordForm, PasswordResetForm
+from django.shortcuts import redirect
+
 from .models import DobboxUser, UserData
 
 UserModel = get_user_model()
@@ -18,21 +21,66 @@ class DobboxAuthenticationForm(AuthenticationForm):
 
 
 class DobboxUserCreationForm(UserCreationForm):
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control bg-transparent', 'placeholder': 'Имейл',
+                                       'required': 'required'}),
+        label="Имейл"
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control bg-transparent', 'placeholder': 'Нова Парола',
+                                          'required': 'required'}),
+        label="Нова парола"
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control bg-transparent', 'placeholder': 'Повторете Паролата',
+                                          'required': 'required'}),
+        label="Повторете паролата"
+    )
+
     class Meta:
         model = UserModel
-        fields = ('email',)
+        fields = ('email', 'password1', 'password2',)
+
+
 
 
 class DobboxUserChangeForm(UserChangeForm):
     class Meta:
         model = UserModel
-        fields = ('email',)  # Добавете или премахнете полета според вашите нужди
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(DobboxUserChangeForm, self).__init__(*args, **kwargs)
+        self.fields['manager'].queryset = DobboxUser.objects.filter(is_staff=True)
 
 
-class UserDataForm(forms.ModelForm):
+class WelcomeUserDataForm(forms.ModelForm):
+    full_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control bg-transparent', 'placeholder': 'Пълно име'}))
+    region = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control bg-transparent', 'placeholder': 'Регион'}))
+    # Добавяне на поле за избор на мениджър
+    manager = forms.ModelChoiceField(
+        queryset=DobboxUser.objects.filter(is_staff=True),
+        required=True,
+        widget=forms.Select(attrs={'class': 'form-control bg-transparent'}),
+        label="Мениджър",
+        empty_label="Изберете мениджър",  # Текст за опцията по подразбиране
+    )
+
     class Meta:
         model = UserData
-        fields = ['name', 'profile_image', 'region', 'gps_long', 'gps_lat']
+        fields = ['full_name', 'region']  # Премахнат е 'manager' от тук
+
+    def save(self, commit=True):
+        instance = super(WelcomeUserDataForm, self).save(commit=False)
+        if commit:
+            instance.save()
+            self.save_m2m()  # Запазва many-to-many данните, ако има такива
+        if 'manager' in self.cleaned_data and self.cleaned_data['manager'] is not None:
+            instance.user.manager = self.cleaned_data['manager']
+            instance.user.save()
+        return instance
+
+
 
 
 class PasswordChangeCustomForm(SetPasswordForm):
@@ -54,7 +102,7 @@ class PasswordChangeCustomForm(SetPasswordForm):
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Паролите не съвпадат")
 
-        user = self.user  # Уверете се, че сте задали 'self.user' във вашата форма
+        user = self.user
         password_validation.validate_password(password2, user)
 
         return password2
--- dobbox_web/authentication/urls.py
+++ dobbox_web/authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from dobbox_web.authentication.views import LoginUserView, logout_user, ResetPasswordView, ResetPasswordConfirmView
+from dobbox_web.authentication.views import LoginUserView, logout_user, ResetPasswordView, ResetPasswordConfirmView, \
+    RegisterUserView, WelcomeUserView
 
 urlpatterns = [
 
@@ -8,5 +9,7 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('password_reset/', ResetPasswordView.as_view(), name='password_reset'),
     path('password_reset_confirm/<uidb64>/<token>/', ResetPasswordConfirmView.as_view(), name='password_reset_confirm'),
+    path('register/', RegisterUserView.as_view(), name='register'),
+    path('welcome/<int:pk>/', WelcomeUserView.as_view(), name='welcome'),
 
 ]
